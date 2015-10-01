a777badeb01a0bfb48f52f7351e6fcf96167acdb
Eduardo Domingos <epcdomingos@gmail.com>
2015-10-01 17:48:32 +0100

    Replace _s code

--- themes/eduardodomingos/footer.php
+++ themes/eduardodomingos/footer.php
@@ -13,13 +13,12 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'eduardodomingos' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'eduardodomingos' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'eduardodomingos' ), 'eduardodomingos', '<a href="http://eduardodomingos.com/" rel="designer">Eduardo Domingos</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="site-info container band">
+            <?php wp_nav_menu( array( 'theme_location' => 'social', 'menu_id' => 'social-menu', 'link_before' => '<span class="hide">', 'link_after' => '</span>' ) ); ?>
+            <small>&copy;<?php echo date('Y'); ?> <?php printf( esc_html__( 'Design and code made with %1$s by %2$s.', 'eduardodomingos' ),'<i class="fa fa-heart"></i>', '<a href="http://eduardodomingos.com" rel="designer">Eduardo Domingos</a>' ); ?></small>
+        </div><!-- .site-info -->
+    </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
