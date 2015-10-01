1bc488d7e4b0e495282b9d144f5ca9d07b51c479
Eduardo Domingos <epcdomingos@gmail.com>
2015-10-01 17:36:01 +0100

    Add custom code to template

--- themes/eduardodomingos/header.php
+++ themes/eduardodomingos/header.php
@@ -22,22 +22,19 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'eduardodomingos' ); ?></a>
+    <a class="skip-link hide" href="#content"><?php esc_html_e( 'Skip to content', 'eduardodomingos' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
-			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site-header" role="banner">
+        <?php if( ! is_single() ) {
+                get_template_part( 'template-parts/content', 'cover' );
+            }
+        ?>
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <div class="site-branding container container--large">
+                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="hide"><?php esc_html_e( 'Primary Menu', 'eduardodomingos' ); ?></span></button>
+                <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container' => false ) ); ?>
+            </div>
+        </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'eduardodomingos' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
