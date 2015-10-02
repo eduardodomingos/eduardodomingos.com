aefe21373e43781ae4301bdca73a0692ae3b8a71
Eduardo Domingos <epcdomingos@gmail.com>
2015-10-02 12:13:56 +0100

    Replace _s code

--- themes/eduardodomingos/template-parts/content-single.php
+++ themes/eduardodomingos/template-parts/content-single.php
@@ -11,25 +11,17 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-			<?php eduardodomingos_posted_on(); ?>
-		</div><!-- .entry-meta -->
+        <?php get_template_part('template-parts/content', 'cover'); ?>
 	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'eduardodomingos' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php eduardodomingos_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <div class="band">
+        <div class="container">
+        	<div class="entry-content">
+                <?php the_content(); ?>
+        	</div><!-- .entry-content -->
+        	<footer class="entry-footer">
+        		<?php eduardodomingos_entry_footer(); ?>
+        	</footer><!-- .entry-footer -->
+        </div><!-- .container -->
+    </div><!-- .band -->
 </article><!-- #post-## -->
 
