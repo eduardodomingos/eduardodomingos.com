2a63b375c6f53e043a0ea44fc4c13060bc508260
Eduardo Domingos <epcdomingos@gmail.com>
2015-10-02 12:49:18 +0100

    Replace _s code

--- themes/eduardodomingos/page.php
+++ themes/eduardodomingos/page.php
@@ -19,14 +19,16 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			        <?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+                <?php if( comments_open() || get_comments_number() ) :?>
+                    <?php // If comments are open or we have at least one comment, load up the comment template. ?>
+                    <div class="band">
+                        <div class="container">
+                            <?php comments_template();?>
+                        </div>
+                    </div>
+                <?php endif; ?>
 
 			<?php endwhile; // End of the loop. ?>
 
