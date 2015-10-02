b497cf8672b200ec4476a01c6ae696317fe698f9
Eduardo Domingos <epcdomingos@gmail.com>
2015-10-02 12:12:51 +0100

    Add transitions and timeago

--- themes/eduardodomingos/assets/src/js/main.js
+++ themes/eduardodomingos/assets/src/js/main.js
@@ -1,5 +1,9 @@
 (function($) {
     $(document).ready(function(){
+        // Transitions Only After Page Load
+        $("body").removeClass("preload");
 
+        // Timeago
+        $("time.timeago").timeago();
     });
 }(jQuery));
