7be7412560be216f06bcb6274c50a33aae39a3db
Eduardo Domingos <epcdomingos@gmail.com>
2015-10-03 21:51:31 +0100

    Concatenate Fastclick to main script

--- themes/eduardodomingos/grunt/concat.js
+++ themes/eduardodomingos/grunt/concat.js
@@ -3,6 +3,7 @@ module.exports = {
         src: [
             //'assets/src/js/vendors/lazysizes.js',
             'assets/src/js/vendors/jquery.timeago.js',
+            'assets/src/js/vendors/fastclick.js',
             'assets/src/js/main.js'
         ],
         dest: 'assets/build/js/main.min.js',
