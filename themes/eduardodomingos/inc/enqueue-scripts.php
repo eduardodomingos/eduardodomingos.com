b4ec29e6cf59fa787eea7099f78318a36fd1491f
Eduardo Domingos <epcdomingos@gmail.com>
2015-10-20 20:04:38 +0100

    Load jQuery and Underscore from CDN

--- themes/eduardodomingos/inc/enqueue-scripts.php
+++ themes/eduardodomingos/inc/enqueue-scripts.php
@@ -4,6 +4,15 @@
  */
 if ( ! function_exists( 'eduardodomingos_scripts' ) ) :
     function eduardodomingos_scripts() {
+
+        // Load jQuery from Google CDN
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js', false, '1.11.3', true);
+
+        // Load Underscore from Cloudflare CDN
+        wp_deregister_script('underscore');
+        wp_register_script('underscore', 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js', false, '1.6.0', true);
+
         // Styles
         wp_enqueue_style( 'icon_fonts', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
         wp_enqueue_style( 'google_fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700|Merriweather' );
