37e96eac83f7694560fc29b5b7170d6e17fad538
Eduardo Domingos <epcdomingos@gmail.com>
2015-10-21 12:24:00 +0100

    Remove commented line

--- themes/eduardodomingos/grunt/concat.js
+++ themes/eduardodomingos/grunt/concat.js
@@ -1,10 +1,9 @@
 module.exports = {
     dist: {
         src: [
-            //'assets/src/js/vendors/lazysizes.js',
             'assets/src/js/vendors/jquery.timeago.js',
             'assets/src/js/vendors/fastclick.js',
-            'assets/src/js/main.js'
+            'assets/src/js/main.js',
         ],
         dest: 'assets/build/js/main.min.js',
     },
