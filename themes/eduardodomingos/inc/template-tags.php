5cdbb6d63b0e7357fe0d3b0029b0183d17a78bda
Eduardo Domingos <epcdomingos@gmail.com>
2015-11-24 18:17:35 +0000

    Temporarily comment most of the method code

--- themes/eduardodomingos/inc/template-tags.php
+++ themes/eduardodomingos/inc/template-tags.php
@@ -45,25 +45,25 @@ if ( ! function_exists( 'eduardodomingos_entry_footer' ) ) :
  */
 function eduardodomingos_entry_footer() {
 	// Hide category and tag text for pages.
-	if ( ! is_front_page() && 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'eduardodomingos' ) );
-		if ( $categories_list && eduardodomingos_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'eduardodomingos' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
+	// if ( ! is_front_page() && 'post' === get_post_type() ) {
+	// 	/* translators: used between list items, there is a space after the comma */
+	// 	$categories_list = get_the_category_list( esc_html__( ', ', 'eduardodomingos' ) );
+	// 	if ( $categories_list && eduardodomingos_categorized_blog() ) {
+	// 		printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'eduardodomingos' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+	// 	}
 
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'eduardodomingos' ) );
-		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'eduardodomingos' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-		}
-	}
+	// 	/* translators: used between list items, there is a space after the comma */
+	// 	$tags_list = get_the_tag_list( '', esc_html__( ', ', 'eduardodomingos' ) );
+	// 	if ( $tags_list ) {
+	// 		printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'eduardodomingos' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+	// 	}
+	// }
 
-	if ( ! is_front_page() && ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link( esc_html__( 'Leave a comment', 'eduardodomingos' ), esc_html__( '1 Comment', 'eduardodomingos' ), esc_html__( '% Comments', 'eduardodomingos' ) );
-		echo '</span>';
-	}
+	// if ( ! is_front_page() && ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+	// 	echo '<span class="comments-link">';
+	// 	comments_popup_link( esc_html__( 'Leave a comment', 'eduardodomingos' ), esc_html__( '1 Comment', 'eduardodomingos' ), esc_html__( '% Comments', 'eduardodomingos' ) );
+	// 	echo '</span>';
+	// }
 
 	edit_post_link(
 		sprintf(
