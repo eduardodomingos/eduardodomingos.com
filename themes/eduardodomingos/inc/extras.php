e46343819b6fa6fef3e5b81f688d66371e441fed
Eduardo Domingos <epcdomingos@gmail.com>
2015-11-25 13:19:17 +0000

    Add share buttons method

--- themes/eduardodomingos/inc/extras.php
+++ themes/eduardodomingos/inc/extras.php
@@ -46,3 +46,41 @@ function eduardodomingos_get_template_part( $slug, $name = null, $data = array()
         $file = "{$slug}.php";
     include locate_template( $file );
 }
+
+/**
+ * Builds the share buttons acrross different articles / pages
+ * @param string $label     What appears before the button
+ * @param string $title     Normally the page title
+ * @param string $description   Normally the page description
+ * @param string $classes       CSS classes to add on the wrapper
+ * @return string (html)
+ */
+function eduardodomingos_share_buttons( $label = 'Share', $url = '', $title = '', $description = '', $classes = '' ) {
+
+    $url = urlencode(html_entity_decode( $url, ENT_COMPAT, 'UTF-8') );
+    $title = rawurlencode(html_entity_decode( $title, ENT_COMPAT, 'UTF-8') );
+    $description = urlencode(html_entity_decode( $description, ENT_COMPAT, 'UTF-8') );
+
+    $html = '<div class="share-this ' . $classes . '">';
+
+    if ( $label != '' ) {
+        $html .= '<span class="label">' . $label . ': </span>';
+    }
+
+    $html .= '<a title="'. esc_html__( 'Share on', 'eduardodomingos' ) .' Facebook" href="https://www.facebook.com/sharer/sharer.php?t=' . $title . '&u=' . $url . '" class="link-share-facebook"><span class="icon--facebook"></span></a>';
+
+    //$html .= '<a title="'. esc_html__( 'Share on', 'eduardodomingos' ) .' Twitter" href="https://twitter.com/intent/tweet?original_referer=' . $url . '&text=' . $title . ': ' . $url . '&via=observadorpt" class="link-share-twitter"><span class="icon-social-twitter"></span></a>';
+
+    $html .= '<a title="'. esc_html__( 'Share on', 'eduardodomingos' ) .' Google+" href="https://plus.google.com/share?url=' . $url . '" class="link-share-gplus"><span class="icon--googleplus"></span></a>';
+
+    $html .= '<a title="'. esc_html__( 'Share on', 'eduardodomingos' ) .' LinkedIn" href="http://www.linkedin.com/shareArticle?mini=true&url=' . $url . '&title=' . $title . '" class="link-share-in"><span class="icon--linkedin"></span></a>';
+
+    $html .= '<a title="'. esc_html__( 'Send by', 'eduardodomingos' ) .' email" href="mailto:?subject=' . $title . '&body='. esc_html__( 'I have just read this interesting article', 'eduardodomingos' ) .': ' . $url . '" class="link-share-email"><span class="icon--email"></span></a>';
+
+    //$html .= '<a title="'. esc_html__( 'Share on', 'eduardodomingos' ) .' WhatsApp" href="whatsapp://send?text=' . $title . ': ' . $url . '" class="link-share-whatsapp" data-action="share/whatsapp/share" style="display:none"><span class="icon--whatsapp"></span></a>';
+
+    $html .= '</div>';
+
+    return $html;
+
+}
